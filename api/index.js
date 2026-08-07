544f3fa825c5cdb0bcf0b95d3fc86352fcdf0640
Ram Sagar Jaiswal <rsjaiswal@q3tech.com>
2026-08-07 17:55:03 +0530

    test: simple function to verify serverless works

--- api/index.js
+++ api/index.js
@@ -1,28 +1,3 @@
-const mongoose = require('mongoose');
-const app = require('./dist/app').default;
-
-let isConnected = false;
-
-const connectToDatabase = async () => {
-  if (isConnected) return;
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    isConnected = true;
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    throw error;
-  }
-};
-
 module.exports = async (req, res) => {
-  try {
-    await connectToDatabase();
-    return app(req, res);
-  } catch (error) {
-    console.error('Handler error:', error);
-    return res.status(500).json({
-      success: false,
-      message: 'Internal server error',
-    });
-  }
+  return res.status(200).json({ success: true, message: 'API is running', url: req.url });
 };
